c6e402cf1a68ba42e9616a2c382fb95edac19ce3
VitorMob <mobhacking100@gmail.com>
2021-12-29 12:51:38 -0300

    [UPD] Error handling

--- src/ps.cpp
+++ src/ps.cpp
@@ -3,20 +3,21 @@
 #include <sstream>
 #include <dirent.h>
 
-
 /**
  * @brief Construct a new Ps:: Ps object
- * 
+ *
  */
 Ps::Ps()
-{ }
+{
+}
 
 /**
  * @brief Destroy the Ps:: Ps object
- * 
+ *
  */
 Ps::~Ps()
-{ }
+{
+}
 
 /**
  * @brief will get all processes from the magic folder / proc
@@ -24,12 +25,21 @@ Ps::~Ps()
  *
  * @param NameProcess pass an array as a parameter to store process names
  * @param PidProcess pass an array as a parameter to store process pids
+ *
+ * @return OPEN_FAIL is /proc not opened else return OPEN_SUCCESS
  */
-void Ps::Reading_DirProcess(std::vector<std::string> &NameProcess,
-                            std::vector<std::string> &PidProcess)
+int Ps::Reading_DirProcess(std::vector<std::string> &NameProcess,
+                           std::vector<std::string> &PidProcess)
 {
+    int status_exit = OPEN_SUCCESS;
     DIR *dir = opendir(PROC);
     struct dirent *dir_read;
+
+    if (dir == NULL)
+    {
+        throw std::runtime_error("Error not open dir /proc");
+        status_exit = OPEN_FAIL;
+    }
 
     while ((dir_read = readdir(dir)) != NULL)
     {
@@ -42,11 +52,13 @@ void Ps::Reading_DirProcess(std::vector<std::string> &NameProcess,
             PidProcess.push_back(dir_read->d_name);
             getline(FSCMDLINE, cmdline);
             if (cmdline.size() == 0)
-                cmdline = "NULL";
+                cmdline = "name not found";
             NameProcess.push_back(cmdline);
             FSCMDLINE.close();
             cmdline.clear();
         }
     }
+
     closedir(dir);
+    return status_exit;
 }
