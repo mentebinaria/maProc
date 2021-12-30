0596757910f7a2e488c38685c397aeecf3749a21
VitorMob <mobhacking100@gmail.com>
2021-12-29 21:14:05 -0300

    [ADD] proclib

--- src/proclib.cpp
+++ src/proclib.cpp
@@ -9,12 +9,13 @@
  * if the remote process were attached successfully but it does not received the SIGSTOP signal
  * it will also returns OPEN_FAIL, otherwise it will return OPEN_SUCCESS
  */
-int RemoteProcess::openProcess(pid_t __pid) 
+int RemoteProcess::openProcess(pid_t __pid)
 {
     proc.pid = __pid;
     status = OPEN_FAIL;
 
-    if (ptrace(PTRACE_ATTACH, proc.pid , NULL, NULL) < 0) {
+    if (ptrace(PTRACE_ATTACH, proc.pid, NULL, NULL) < 0)
+    {
         return status;
         // error is set in errno
     }
@@ -23,13 +24,13 @@ int RemoteProcess::openProcess(pid_t __pid)
 
     int wstatus;
     waitpid(proc.pid, &wstatus, 0);
-    
-    if (WIFSTOPPED(wstatus)) {
+
+    if (WIFSTOPPED(wstatus))
+    {
         // process has been attached and stopped, continue normally
         ptrace(PTRACE_CONT, proc.pid, NULL, NULL);
         status = OPEN_SUCCESS;
         return status; // success :)
-
     }
 
     return status;
@@ -40,22 +41,23 @@ int RemoteProcess::openProcess(pid_t __pid)
  * @param start offset to be read into the remote process
  * @param Data* data class to save the memory data
  * @return int
- * 
+ *
  * If the remote memory cannot be read, it will returns READ_FAIL otherwise it will returns READ_SUCESS. The error cause can be found at the
  * errno variable, which is set by ptrace.
  */
-int RemoteProcess::readMem(off_t start, Data* data) 
+int RemoteProcess::readMem(off_t start, Data *data)
 {
     if (status != OPEN_SUCCESS)
         return READ_FAIL;
 
     long b;
-    for (uint i = 0; i < data->size; ++i) {
-        b = ptrace(PTRACE_PEEKDATA, proc.pid, start + i, NULL); 
+    for (uint i = 0; i < data->size; ++i)
+    {
+        b = ptrace(PTRACE_PEEKDATA, proc.pid, start + i, NULL);
         if (b == -1)
             return READ_FAIL;
 
-        data->write((uint8_t) b);
+        data->write((uint8_t)b);
     }
 
     return READ_SUCCESS;
@@ -63,21 +65,22 @@ int RemoteProcess::readMem(off_t start, Data* data)
 
 /**
  * @brief Write into a remote process memory
- * 
- * @param start offset to be written into 
+ *
+ * @param start offset to be written into
  * @param data class with the target data to be write
- * @return int 
- * 
- * If the remote memory cannot be written, it will returns WRITE_FAIL, otherwise WRITE_SUCESS. The error cause can be found at the 
+ * @return int
+ *
+ * If the remote memory cannot be written, it will returns WRITE_FAIL, otherwise WRITE_SUCESS. The error cause can be found at the
  * errno variable, which is set by ptrace.
  */
-int RemoteProcess::writeMem(off_t start, Data* data) 
+int RemoteProcess::writeMem(off_t start, Data *data)
 {
     if (status != OPEN_SUCCESS)
         return WRITE_FAIL;
-    
+
     long s;
-    for (uint i = 0; i < data->size; ++i) {
+    for (uint i = 0; i < data->size; ++i)
+    {
         s = ptrace(PTRACE_POKEDATA, proc.pid, start + i, data->read());
         if (s < 0)
             return WRITE_FAIL;
@@ -86,7 +89,7 @@ int RemoteProcess::writeMem(off_t start, Data* data)
     return WRITE_SUCCESS;
 }
 
-Data::Data(uint __size) 
+Data::Data(uint __size)
 {
     size = __size;
     buff = new uint8_t[size];
@@ -98,14 +101,16 @@ Data::~Data()
     delete[] buff;
 }
 
-void Data::write(uint8_t b) {
+void Data::write(uint8_t b)
+{
     if (curr++ > size) // not enough sspace
-        return; 
+        return;
 
     *(buff++) = b;
 }
 
-uint8_t Data::read() {
+uint8_t Data::read()
+{
     curr = curr % size; // cyclic reading
     return buff[curr++];
 }
